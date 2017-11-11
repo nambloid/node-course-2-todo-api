546369d61046c23c6ebdf5efc822780bfdb30051
Emil Pettersson <emil.pettersson05@gmail.com>
2017-11-11 16:10:29 +0200

    Modifed some functions to async await

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -181,7 +181,7 @@ describe('PATCH /todos/:id', () => {
             .expect((res) => {
                 expect(res.body.todo.text).toBe(text);
                 expect(res.body.todo.completed).toBe(true);
-                //expect(res.body.todo.completedAt).toBeA(Number);
+                expect(typeof res.body.todo.completedAt).toBe('number');
             })
             .end(done);
     });
@@ -300,28 +300,28 @@ describe('POST /users', () => {
 
 describe('POST /users/login', () => {
 
-    // it('should login user and return auth token', (done) => {
-    //     request(app)
-    //         .post('/users/login')
-    //         .send({
-    //             email: users[1].email,
-    //             password: users[1].password
-    //         })
-    //         .expect(200)
-    //         .expect((res) => {
-    //             expect(res.header['x-auth']).toBeTruthy();
-    //         })
-    //         .end((err, res) => {
-    //             if (err) return done(err);
-    //             User.findById(users[1]._id).then((user) => {
-    //                 expect(user.tokens[1]).toContain({
-    //                     access: 'auth',
-    //                     token: res.headers['x-auth']
-    //                 });
-    //                 done();
-    //             }).catch((e) => done(e));
-    //         });
-    // });
+    it('should login user and return auth token', (done) => {
+        request(app)
+            .post('/users/login')
+            .send({
+                email: users[1].email,
+                password: users[1].password
+            })
+            .expect(200)
+            .expect((res) => {
+                expect(res.header['x-auth']).toBeTruthy();
+            })
+            .end((err, res) => {
+                if (err) return done(err);
+                User.findById(users[1]._id).then((user) => {
+                    expect(user.toObject().tokens[1]).toMatchObject({
+                        access: 'auth',
+                        token: res.headers['x-auth']
+                    });
+                    done();
+                }).catch((e) => done(e));
+            });
+    });
 
     it('should reject invalid login', (done) => {
         request(app)
